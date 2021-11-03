0b952f214e129bb79fb5d75d4430f6fe33e7d967
nfrederick023 <nfrederick023@gmail.com>
2021-11-03 16:11:29 -0400

    Bug fixes and Clear()
    Changes:
    - added clear() method
    - fixed bug that caused add() to end prematurely and not add objects
    - removed objCount since sizeCount functions identically
    - added some more tests to driver
    - changed add()'s return type from void to boolean which indicates whether an obj was added or not
    - renamed get() to contains() to match Java implementation
    - changed contains()'s return type from T to boolean, as it functions the same

--- src/Driver.java
+++ src/Driver.java
@@ -5,57 +5,52 @@ public class Driver {
         HashSetAmplifier<User> test = new HashSetAmplifier<User>();
         System.out.println("Initialization");
 
+        // tests:
+
+        int userAdded = 250;
+        for (int i = 0; i < userAdded; i++) {
+            User user1 = new User();
+            test.add(user1);
+
+        }
+        if (test.size() == userAdded) {
+            System.out.println("Add() ran successfully!");
+        } else {
+            System.out.println("Error in add() function!");
+        }
+
+        test.clear();
+
+        if (test.size() == 0) {
+            System.out.println("clear() ran succesfully");
+        } else {
+            System.out.println("Error in clear() function!");
+        }
+
         User user = new User();
-        user.age = 10;
-        //System.out.println(user.hashCode());
-
-        User user2 = new User();
-        user2.age = 12;
-        //System.out.println(user2.hashCode());
-
-        user.age = 5;
-        //System.out.println(user.hashCode());
-
-        User user3 = new User();
-        User user4 = new User();
-        User user5 = new User();
-        User user6 = new User();
-        User user7 = new User();
-        user7.age = 50;
-        User user8 = new User();
-        User user9 = new User();
-        User user10 = new User();
-
 
         test.add(user);
-        test.add(user2);
-        test.add(user);
-        test.add(user3);
-        test.add(user4);
-        test.add(user5);
-        System.out.println(test.arrSize);
-        test.add(user6);
-        test.add(user7);
-        test.add(user8);
-        test.add(user9);
-        test.add(user10);
-        System.out.println(test.arrSize);
 
+        if (test.contains(user)) {
+            System.out.println("contains() ran succesfully");
+        } else {
+            System.out.println("Error in contains() function!");
+        }
 
+        test.remove(user);
 
-        test.get(user9);
-        System.out.println(test.getSize());
-
-        System.out.println(test.get(user7).age);
-        test.remove(user7);
-        if(test.get(user7) == null) {
-            System.out.println("success");
+        if (!test.contains(user)) {
+            System.out.println("remove() ran succesfully");
+        } else {
+            System.out.println("Error in remove() function!");
         }
 
     }
 
     public static class User {
         int age;
-        public User() {}
+
+        public User() {
+        }
     }
 }
